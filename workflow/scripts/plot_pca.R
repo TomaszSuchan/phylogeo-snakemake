0e223b929547fcdc2cb9b51573765757504ee467
TomaszSuchan <9158028+TomaszSuchan@users.noreply.github.com>
2025-10-23 00:11:28 +0200

    column name -> number

--- workflow/scripts/plot_pca.R
+++ workflow/scripts/plot_pca.R
@@ -11,7 +11,7 @@ output_file  <- snakemake@output[[1]]
 
 pc1 <- as.numeric(snakemake@params[["pc1"]])
 pc2 <- as.numeric(snakemake@params[["pc2"]])
-color_by <- snakemake@params[["color_by"]]
+color_by_name <- snakemake@params[["color_by"]]  # <-- column name from config
 
 # Read inputs
 eigenvecs2 <- read.table(eigvecs_file)
@@ -20,13 +20,19 @@ eigenvecs <- eigenvecs2[, -c(1,2)]
 eigenvals <- scan(eigvals_file)
 popdata <- read.table(popdata_file, header = TRUE)
 
+# Convert color_by from column name to index (as required by plot_pca)
+if (!color_by_name %in% names(popdata)) {
+  stop(paste("Column", color_by_name, "not found in popdata"))
+}
+color_by_index <- which(names(popdata) == color_by_name)
+
 # Generate PCA plot using your package function
 plt_pca <- plot_pca(
   individuals = individuals,
   eigenvecs = eigenvecs,
   eigenvals = eigenvals,
   popdata = popdata,
-  color_by = color_by,  # now column name (string)
+  color_by = color_by_index,  # <-- numeric index
   pc1 = pc1,
   pc2 = pc2
 )
